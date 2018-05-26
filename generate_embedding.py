79bf3b85dea7e37980013adade740fedfe0c65ae
krohak <rohaksinghal14@gmail.com>
2018-05-26 14:50:08 +0530

    temperature experiments

--- generate_embedding.py
+++ generate_embedding.py
@@ -93,12 +93,12 @@ for topic in topics:
     def sample(preds, temperature=1.0):
         # helper function to sample an index from a probability array
         # preds = preds[1:]
-        # preds = np.asarray(preds).astype('float64')
-        # preds = np.log(preds) / temperature
-        # exp_preds = np.exp(preds)
-        # preds = exp_preds / np.sum(exp_preds)
-        # probas = np.random.multinomial(1, preds, 1)
-        return np.argmax(preds)
+        preds = np.asarray(preds).astype('float64')
+        preds = np.log(preds) / temperature
+        exp_preds = np.exp(preds)
+        preds = exp_preds / np.sum(exp_preds)
+        probas = np.random.multinomial(1, preds, 1)
+        return np.argmax(probas)
 
 
     def on_epoch_end(epoch, logs):
@@ -107,7 +107,7 @@ for topic in topics:
         print('----- Generating text after Epoch: %d' % epoch)
 
         start_index = np.random.randint(0, len(funny_doc) - maxlen - 1)
-        for diversity in [0.2]: # 0.5, 1.0, 1.2
+        for diversity in [0.2, 0.5, 1.0, 1.2]:
             print('----- diversity:', diversity)
 
             generated = ''
