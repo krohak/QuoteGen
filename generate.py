4101ebec68dcfa812ee8e544cc41dba54530b3df
krohak <rohaksinghal14@gmail.com>
2018-05-27 01:35:56 +0530

    wat

--- generate.py
+++ generate.py
@@ -53,17 +53,6 @@ for topic in topics[1:]:
 
 
 
-# # Text Generation using Word Embeddings
-def sample(preds, temperature=1.0):
-# helper function to sample an index from a probability array
-    preds = np.asarray(preds).astype('float64')
-    preds = np.log(preds) / temperature
-    exp_preds = np.exp(preds)
-    preds = exp_preds / np.sum(exp_preds)
-    probas = np.random.multinomial(1, preds, 1)
-    return np.argmax(probas)
-
-
 def on_epoch_end(sentence, model, maxlen = 10):
     # for diversity in [1.0]: # 0.2, 0.5, 1.2
     predicted = ''
@@ -73,7 +62,7 @@ def on_epoch_end(sentence, model, maxlen = 10):
 
         preds = model.predict(x_pred, verbose=0)
         preds = preds[0]
-        next_index =  np.argmax(preds) #sample(preds,diversity)
+        next_index =  np.argmax(preds)
         next_char = index_word[next_index]
 
         sentence = np.append(sentence, next_index)
