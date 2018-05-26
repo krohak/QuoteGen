d114e45b79dc08aba6de304173863b8b87ccbea9
krohak <rohaksinghal14@gmail.com>
2018-05-26 15:56:17 +0530

    trying to generate with two topics

--- generate_embedding.py
+++ generate_embedding.py
@@ -24,7 +24,7 @@ t = Tokenizer()
 t.fit_on_texts(quotes)
 vocab_size = len(t.word_index) + 1
 
-
+maxlen = 10
 
 # In[12]:
 
@@ -34,108 +34,74 @@ index_word = index_word.item()
 
 topics = [ #'death' , 'family', 'freedom' , 'funny', 'life' ,
     	#'love', 
-	'happiness', 
+	'happiness', 'death'
 	#'success', 'science', 'politics'
 ]
+
+# In[13]:
+with open('data/%s.txt'%topics[0],'r') as funnyfile:
+    funnyquotes = funnyfile.readlines()
+
+encoded_docs = t.texts_to_sequences(funnyquotes)
+funny_doc = encoded_docs[0]
+
+model_list = []
 
 
 # ## Do for all docs
 for topic in topics:
-
-    # In[13]:
-
-    with open('data/%s.txt'%topic,'r') as funnyfile:
-        funnyquotes = funnyfile.readlines()
-
-
-    # In[14]:
-
-    encoded_docs = t.texts_to_sequences(funnyquotes)
-    funny_doc = encoded_docs[0]
-
-    
-    # In[15]:
-    maxlen = 10
-    
-    # # Text Generation using Word Embeddings
-
-
-    # In[20]:
     model_funny = Model(vocab_size,topic)
     model = model_funny.load_model()
-    '''
-    e = Embedding( vocab_size, 100, 
-        #weights=[embedding_matrix], input_length=maxlen, trainable=False
-        )
+    model_list.append(model)
 
 
-    # In[27]:
-
-    model = Sequential()
-    model.add(e)
-
-    model.add(LSTM(100))
-    model.add(Dense(vocab_size))
     
-
-    # In[30]:
-
-    model.add(Activation('softmax'))
-    optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
-    
-    filename = "QG-weights-%s.hdf5"%topic
-    model.load_weights(filename)
-    
-    model.compile(
-        loss='categorical_crossentropy',
-        optimizer=optimizer,
-        metrics=['accuracy'])
-    print(model.summary())
-    '''
-
-    # In[32]:
-
-    def sample(preds, temperature=1.0):
-        # helper function to sample an index from a probability array
-        # preds = preds[1:]
-        # preds = np.asarray(preds).astype('float64')
-        # preds = np.log(preds) / temperature
-        # exp_preds = np.exp(preds)
-        # preds = exp_preds / np.sum(exp_preds)
-        # probas = np.random.multinomial(1, preds, 1)
-        return np.argmax(preds)
+# # Text Generation using Word Embeddings
+def sample(preds, temperature=1.0):
+# helper function to sample an index from a probability array
+preds = np.asarray(preds).astype('float64')
+preds = np.log(preds) / temperature
+exp_preds = np.exp(preds)
+preds = exp_preds / np.sum(exp_preds)
+probas = np.random.multinomial(1, preds, 1)
+return np.argmax(probas)
 
 
-    def on_epoch_end(epoch, logs):
-        # Function invoked at end of each epoch. Prints generated text.
+def on_epoch_end(epoch, logs, sentence, model):
+print()
+print('----- Generating text after Epoch: %d' % epoch)
+
+
+for diversity in [1.0]: #0.2, 0.5, 1.2
+    print('----- diversity:', diversity)
+
+    generated = ''
+    print(sentence)
+    generated.join([str([index_word[value]]).join(' ') for value in sentence])
+    print('----- Generating with seed: %s'%[index_word[word] for word in sentence])
+
+    for i in range(20):
+        x_pred = np.reshape(sentence,(1, -1))
+
+        preds = model.predict(x_pred, verbose=0)
+        preds = preds[0]
+        next_index = np.argmax(preds)
+        next_char = index_word[next_index]
+
+        generated.join(str(next_char))
+        sentence = np.append(sentence, next_index)
+
+        sys.stdout.write(next_char)
+        sys.stdout.write(" ")
+        sys.stdout.flush()
         print()
-        print('----- Generating text after Epoch: %d' % epoch)
+        
+        return sentence
 
-        start_index = np.random.randint(0, len(funny_doc) - maxlen - 1)
-        for diversity in [1.0]: #0.2, 0.5, 1.2
-            print('----- diversity:', diversity)
+        
+        
+start_index = np.random.randint(0, len(funny_doc) - maxlen - 1)
+sentence = funny_doc[start_index: start_index + maxlen]
 
-            generated = ''
-            sentence = [3, 46, 112, 786, 49, 5, 63, 41, 201, 28]
-            #funny_doc[start_index: start_index + maxlen]
-            print(sentence)
-            generated.join([str([index_word[value]]).join(' ') for value in sentence])
-            print('----- Generating with seed: %s'%[index_word[word] for word in sentence])
-
-            for i in range(20):
-                x_pred = np.reshape(sentence,(1, -1))
-
-                preds = model.predict(x_pred, verbose=0)
-                preds = preds[0]
-                next_index = sample(preds, diversity)
-                next_char = index_word[next_index]
-
-                generated.join(str(next_char))
-                sentence = np.append(sentence, next_index)
-
-                sys.stdout.write(next_char)
-                sys.stdout.write(" ")
-                sys.stdout.flush()
-            print()
-
-    on_epoch_end(1,1)
+for model in model_list:
+    sentence = on_epoch_end(1,1,sentence,model)
